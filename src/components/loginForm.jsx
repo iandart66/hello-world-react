07faf37d60bb06ea1c86afd9881a43453cd0bbad
iandart66 <ian.dart@idc.technology>
2019-12-11 06:12:49 +0000

    LoginForm amendments

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -3,16 +3,33 @@ import React, { Component } from "react";
 class LoginForm extends Component {
   render() {
     return (
-      <div>
-        <h1>Logins</h1>
-        <form>
+      <div className="text-center">
+        <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
+        <form className="form-signin">
           <div className="form-group">
-            <label htmlFor="username">Username</label>
-            <input type="text" id="username" className="form-control" />
+            <label htmlFor="username" class="sr-only">
+              Username
+            </label>
+            <input
+              type="text"
+              id="username"
+              className="form-control"
+              placeholder="Username"
+            />
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
-            <input type="text" id="password" className="form-control" />
+            <input
+              type="text"
+              id="password"
+              className="form-control"
+              placeholder="Password"
+            />
+          </div>
+          <div class="checkbox mb-3">
+            <label>
+              <input type="checkbox" value="remember-me" /> Remember me
+            </label>
           </div>
           <button className="btn btn-primary">Login</button>
         </form>
