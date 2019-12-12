38d2090c6b288cdfe532b8ec737c2b6126bee275
iandart66 <ian.dart@idc.technology>
2019-12-12 06:18:48 +0000

    Navbar Modifications

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -3,36 +3,42 @@ import React, { Component } from "react";
 class LoginForm extends Component {
   render() {
     return (
-      <div className="text-center">
-        <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
-        <form className="form-signin">
-          <div className="form-group">
-            <label htmlFor="username" class="sr-only">
-              Username
-            </label>
-            <input
-              type="text"
-              id="username"
-              className="form-control"
-              placeholder="Username"
-            />
+      <div className="container">
+        <div className="row text-center">
+          <div className="col-sm-4"></div>
+          <div className="col-sm-4">
+            <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
+            <form className="form-signin">
+              <div className="form-group">
+                <label htmlFor="username" class="sr-only">
+                  Username
+                </label>
+                <input
+                  type="text"
+                  id="username"
+                  className="form-control"
+                  placeholder="Username"
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="password">Password</label>
+                <input
+                  type="password"
+                  id="password"
+                  className="form-control"
+                  placeholder="Password"
+                />
+              </div>
+              <div class="checkbox mb-3">
+                <label>
+                  <input type="checkbox" value="remember-me" /> Remember me
+                </label>
+              </div>
+              <button className="btn btn-primary">Login</button>
+            </form>
           </div>
-          <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input
-              type="text"
-              id="password"
-              className="form-control"
-              placeholder="Password"
-            />
-          </div>
-          <div class="checkbox mb-3">
-            <label>
-              <input type="checkbox" value="remember-me" /> Remember me
-            </label>
-          </div>
-          <button className="btn btn-primary">Login</button>
-        </form>
+          <div className="col-sm-4"></div>
+        </div>
       </div>
     );
   }
--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -5,14 +5,23 @@ import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/loginForm">Login</Link>
-      </li>
-    </ul>
+    <nav className="navbar navbar-expand-sm bg-light">
+      <a className="navbar-brand" href="#">
+        iDC Mobile
+      </a>
+      <ul className="navbar-nav">
+        <li>
+          <Link className="nav-link" to="/">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link className="nav-link" to="/loginForm">
+            Login
+          </Link>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
