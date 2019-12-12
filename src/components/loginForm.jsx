b65c531285ca963c764e0f41c0561c079a7edf42
iandart66 <ian.dart@idc.technology>
2019-12-12 22:38:09 +0000

    Login Form image amended

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,18 +1,31 @@
 import React, { Component } from "react";
+import Logo from "./Logo.svg";
+
+console.log(Logo);
 
 class LoginForm extends Component {
   render() {
     return (
       <div className="container">
-        <div className="row text-center">
+        <div>
+          <a className="navbar-brand mx-auto" href="#">
+            <img
+              src={Logo}
+              alt="Logo"
+              width="100px"
+              height="100px"
+              className="img-fluid"
+            />
+          </a>
+        </div>
+
+        <div className="row">
           <div className="col-sm-4"></div>
           <div className="col-sm-4">
             <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
             <form className="form-signin">
               <div className="form-group">
-                <label htmlFor="username" class="sr-only">
-                  Username
-                </label>
+                <label htmlFor="username">Username</label>
                 <input
                   type="text"
                   id="username"
